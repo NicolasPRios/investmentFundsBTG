895878da2a0f53e4bb095f11eae95a2cdd38cb7d
nicolas <ing.nicolaspr@gmail.com>
2026-03-17 21:41:19 -0500

    Espacio de trabajo

---
+++ src/test/java/com/investmentFundsBTG/investmentFunds/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investmentFundsBTG.investmentFunds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
